eff797dec0f797d874e516d717469f4f10d46d86
AhmedMahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-09 16:22:16 +0200

    Add AddNoteBottomSheet with input fields and custom button; create CustomTextField widget for better reusability

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -11,7 +11,12 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
         onPressed: () {
-          showModalBottomSheet(context: context, builder: (context){
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            context: context, 
+            builder: (context){
             return const AddNoteBottomSheet(); 
           });
         } , 
