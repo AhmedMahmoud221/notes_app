96b08a1aaacbf8a03a647f38a917181463018f6f
AhmedMahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-10 11:12:30 +0200

    Refactor NoteModel class to include Hive annotations and add generated adapter; update pubspec.yaml for Hive dependencies and version upgrades

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,17 @@
-class NoteModel
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject
 {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String sibTitle;
+  @HiveField(2)
   final String data;
+  @HiveField(3)
   final int color;
 
   NoteModel({
