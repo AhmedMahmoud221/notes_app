b42effb334b6eb79930582443af9ae0a5ae67682
AhmedMahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-10 09:07:35 +0200

    Add NoteModel class and update EditNoteViewBody with CustomTextField widgets; modify pubspec.yaml for Hive dependencies and build_runner

---
+++ lib/models/note_model.dart
@@ -0,0 +1,15 @@
+class NoteModel
+{
+  final String title;
+  final String sibTitle;
+  final String data;
+  final int color;
+
+  NoteModel({
+    required this.title, 
+    required this.sibTitle, 
+    required this.data, 
+    required this.color,
+    }
+  );
+}
