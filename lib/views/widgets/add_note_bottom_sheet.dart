7c241a1dff005739601a078345f5a2bd519ff1e9
AhmedMahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-11 10:05:23 +0200

    Refactor AddNoteBottomSheet padding; remove unused modal_progress_hud_nsn import

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
@@ -32,7 +31,10 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: EdgeInsets.only(
+                left: 16, 
+                right: 16, 
+                bottom: MediaQuery.of(context).viewInsets.bottom),
               child: const SingleChildScrollView(
                 child: AddNoteForm()),
             ),
